396f97ef37343bf77f6439bf1fd4a6f984c5ac6a
Paamzzz <pamamanciogoulart@gmail.com>
2026-05-19 18:31:20 -0300

    chore: adicionar enviroment.example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+
+  groqApiKey: 'your_api_key_here',
+  groqModel: 'llama-3.3-70b-versatile',
+
+  firebaseConfig: {
+    apiKey: "AIzaSyA7jxdYk-i9NHmTkb4Cms3cxSntIskblUM",
+    authDomain: "projetoflashcards3.firebaseapp.com",
+    projectId: "projetoflashcards3",
+    storageBucket: "projetoflashcards3.firebasestorage.app",
+    messagingSenderId: "249600256129",
+    appId: "1:249600256129:web:8870fa8e798ecc2df44580",
+    measurementId: "G-H7Z4VJVHTE"
+  }
+};
