edb0c4d444a95db672eddac2a8b2842a93171678
Paamzzz <pamamanciogoulart@gmail.com>
2026-06-02 20:41:07 -0300

    feat: adicionar função pegarDecks()

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Observable, firstValueFrom } from 'rxjs';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Deck } from '../interfaces/deck'
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeckService {
+  private firestore = inject(Firestore);
+
+  pegarDecks(): Observable<Deck[]>{
+     const decks = collection(this.firestore, 'decks' );
+     return collectionData(decks, { idField:'id' }) as Observable<Deck[]>;
+     // É necessário colocarmos o "idField" pois o firestore não traz ele automaticamente
+     // Então ao fazer um collectionData(), traria tudo (nome, tipo...) menos id
+  }
+
   
 }
