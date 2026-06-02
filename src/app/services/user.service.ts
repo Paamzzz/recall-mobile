08e0fc7b41cc0fe01616eed5e0f196ba0d88e8b9
Paamzzz <pamamanciogoulart@gmail.com>
2026-06-01 21:50:02 -0300

    feat: criar função de pegarUserProfile no user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { doc, docData, setDoc, Firestore } from '@angular/fire/firestore'
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  private firestore = inject(Firestore);
+
+  pegarUserProfile(uid: string): Observable<User> {
+     const docUser = doc(this.firestore, 'users', uid);
+     return docData(docUser) as Observable<User>;
+  }
+
+
 }
