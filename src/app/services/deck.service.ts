2a0f45826c0843af5ea102d739f88b334dcc298a
Paamzzz <pamamanciogoulart@gmail.com>
2026-06-03 17:17:49 -0300

    feat: adiciona 'pegarRandomDeck()' no deck.service

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -16,5 +16,13 @@ export class DeckService {
      // Então ao fazer um collectionData(), traria tudo (nome, tipo...) menos id
   }
 
+  async pegarRandomDeck(): Promise<Deck> { 
+     // precisamos ESPERAR a lista com os decks chegarem para sortearmos um, por isso o await 
+     //Inclusive, ele não combina com o 'Observable', já que ele sempre vai estar atualizando e precisamos de um valor fixo
+     const decks = await firstValueFrom(this.pegarDecks());
+     const indice = Math.floor(Math.random() * decks.length);
+     const deckAleatorio = decks[indice];
+     return deckAleatorio;
+  }
   
 }
